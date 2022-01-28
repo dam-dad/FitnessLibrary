88598427b82a70606d80df1c7539b9dab761d1b4
Javi0108 <59823740+Javi0108@users.noreply.github.com>
2022-01-28 18:34:58 +0000

    MenuBar

--- src/main/java/dad/fitnesslibrary/activity/App.java
+++ src/main/java/dad/fitnesslibrary/activity/App.java
@@ -1,20 +1,23 @@
 package dad.fitnesslibrary.activity;
 
+import dad.fitnesslibrary.app.RoutineActivityController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
-	private ListViewController controller;
+//	private ListViewController controller;
+	private RoutineActivityController controller;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 
-		controller = new ListViewController();
+//		controller = new ListViewController();
+		controller = new RoutineActivityController();
 		
-		Scene scene = new Scene(controller.getRoot());
+		Scene scene = new Scene(controller.getView());
 		
 		primaryStage.setTitle("APIREST_Trials");
 		primaryStage.setScene(scene);
--- src/main/java/dad/fitnesslibrary/app/RoutineActivityController.java
+++ src/main/java/dad/fitnesslibrary/app/RoutineActivityController.java
@@ -4,30 +4,40 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 public class RoutineActivityController implements Initializable {
 
 
-
     @FXML
-    private Tab activityTab;
-
+    private BorderPane view;
+	
     @FXML
     private TabPane mainView;
 
     @FXML
+    private Tab activityTab;
+    
+    @FXML
     private Tab routineTab;
 
+    @FXML
+    private Button buscarButton;
 
     @FXML
-    private BorderPane view;
+    private TextField busquedaText;
 
+    @FXML
+    private ComboBox<?> parteCuerpoCombo;
     
 	public RoutineActivityController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RoutineActivityView.fxml"));
@@ -39,6 +49,11 @@ public class RoutineActivityController implements Initializable {
 		// TODO Auto-generated method stub
 
 	}
+	
+    @FXML
+    void onBuscarButtonAction(ActionEvent event) {
+
+    }
 	
 	public BorderPane getView() {
 		return view;
