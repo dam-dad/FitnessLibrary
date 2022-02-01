f7a3d9adeb497f8464c3a48cc901b7a33bd8d985
VegasBeats <j.j.13sg10@gmail.com>
2022-02-01 22:20:12 +0000

    Changed the program's language to English and cleaned the imports

--- src/main/java/dad/fitnesslibrary/activity/MenuBarController.java
+++ src/main/java/dad/fitnesslibrary/activity/MenuBarController.java
@@ -1,36 +1,17 @@
 package dad.fitnesslibrary.activity;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.concurrent.ExecutionException;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import dad.fitnesslibrary.classes.TipoComboBox;
-import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextField;
 
 public class MenuBarController implements Initializable {
-
-	private Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-	private Type stringListType = new TypeToken<List<String>>() {
-	}.getType();
 
 	@FXML
     private MenuBar view;
